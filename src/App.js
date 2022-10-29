35412439c9b3b4db02d4a2410a9f692327edf8d4
bbbozhena <bbbozhenka@gmail.com>
2022-10-29 20:59:05 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <svg/>
+    <div className="wrapper clear">
+      <header className='d-flex justify-between align-center p-40' >
+        <div className='d-flex alight-center'>
+        <img width={40} height={40} src="/img/logo.png"/>
         <div>
-          <h3>Real Sneakers</h3>
+          <h3 className='text-uppercase'>Real Sneakers</h3>
           <p>Магазин найкращого взуття</p>
         </div>
-        <ul>
-          <li> <svg/> <span>1200грн</span> </li>
-          <li> <svg/> </li>
+        </div>
+        <ul className='d-flex'>
+          <li className='mr-30'> <img width={18} height={18} src="/img/cart.svg"/> <span>1200грн</span> </li>
+          <li> <img width={18} height={18} src="/img/user.svg"/> </li>
         </ul>
       </header>
+
+      <div className='content p-40'>
+        <h1>All shoes</h1>
+      </div>
  
     </div>
   );
